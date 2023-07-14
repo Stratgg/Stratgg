db46982d7a0034525893739766f4b6c672ab3049
Marshall Burns <marshallbcodes@gmail.com>
2023-07-14 17:18:22 -0500

    Build basic 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<!-- This error page is displayed when a page cannot not found -->
+
+
+<div class="pageNotFoundContainer">
+  <h1 class="pageNotFoundHeading">404: Page Not Found</h1>
+  <p class="pageNotFoundPara">Sorry this page could not be found. Please try again</p>
+  <!-- TODO possibly add an image or GIF -->
+</div>
