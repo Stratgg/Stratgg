781099177bda53552e463bccd2af489c76a87492
Marshall Burns <marshallbcodes@gmail.com>
2023-07-14 21:45:14 -0500

    Add button that takes user home

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,8 +1,14 @@
 <!-- This error page is displayed when a page cannot not found -->
 
+<script lang="ts">
+ const takeHome = () => {
+    window.location.href = "/";
+  };
+</script>
 
 <div class="pageNotFoundContainer">
   <h1 class="pageNotFoundHeading">404: Page Not Found</h1>
-  <p class="pageNotFoundPara">Sorry this page could not be found. Please try again</p>
+  <p class="pageNotFoundPara">Sorry this page could not be found. Please try again.</p>
   <!-- TODO possibly add an image or GIF -->
+  <button class="pageNotFoundHomeBtn" on:click={takeHome}>Go Home</button>
 </div>
