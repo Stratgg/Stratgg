9f0e358db04f8605d5dad0789ef56b34b163ff0f
Marshall Burns <marshallbcodes@gmail.com>
2023-07-14 23:11:23 -0500

    Change button font size

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -7,12 +7,12 @@
 </script>
 
 <div id="pageNotFoundFlexContaier" class="text-center border-solid border-2 border-red-500"  >
-  <div id="pageNotFoundContainer" class="">
-    <h1 id="pageNotFoundHeading " class="text-9xl">Oops!</h1>
+  <div id="pageNotFoundContainer" >
+    <h1 id="pageNotFoundHeading " class="text-9xl mb-5" >Oops!</h1>
     <h3 id="pageNotFoundSubHeading" class="text-5xl">404 - Page Not Found</h3>
     <p id="pageNotFoundPara" class="text-3xl">Sorry this page could not be found. Please try again.</p>
     <!-- TODO possibly add an image or GIF -->
-    <button id="pageNotFoundHomeBtn" class="bg-coolBlue"   on:click={takeHome}>Go To Homepage</button>
+    <button id="pageNotFoundHomeBtn" class="bg-coolBlue text-2xl"   on:click={takeHome}>Go To Homepage</button>
   </div>
 </div>
 
