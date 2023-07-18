0f301b81d4fb68f8c1c152205223484b7ff3ef98
Stratgg-Admin <139698083+Stratgg-Admin@users.noreply.github.com>
2023-07-18 07:26:51 -0500

    Revert "Development"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,1 @@
-<!-- This modifies the title of this page -->
-<svelte:head>
-  <title>StratGG / Home</title>
-</svelte:head>
-<script>
-  import Footer from "$lib/components/Footer.svelte";
-	import Nav from "$lib/components/Nav.svelte";
-</script>
-
-<Nav/>
-<Footer/>
+<h1>STRATGG</h1>
