132ab65f90b43e1ed69f7f75cceb387e256f554d
Marshall Burns <marshallbcodes@gmail.com>
2023-07-20 07:30:01 -0500

    Add assets/add favicon to pages

--- src/routes/FAQ/+page.svelte
+++ src/routes/FAQ/+page.svelte
@@ -1,11 +1,13 @@
 <!-- This modifies the title of this page -->
 <svelte:head>
   <title>StratGG / FAQ</title>
-</svelte:head>
+  <link rel="icon" href={favicon} />
+ </svelte:head>
 
 <script>
   import Footer from "$lib/components/Footer.svelte";
 	import Nav from "$lib/components/Nav.svelte";
+  import favicon from "$lib/assets/favicons/favicon-32x32.ico";
 </script>
 
 <Nav/>
