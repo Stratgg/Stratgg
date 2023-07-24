2d35273bccabff8fd1c99968521701b5c670221a
Marshall Burns <marshallbcodes@gmail.com>
2023-07-24 14:45:41 -0500

    Add temp asset/minor changes to player profile

--- client/src/routes/esports/[player_id]/+page.svelte
+++ client/src/routes/esports/[player_id]/+page.svelte
@@ -7,25 +7,25 @@
   import Footer from "$lib/components/Footer.svelte";
 	import Nav from "$lib/components/Nav.svelte";
   import favicon from "$lib/assets/favicons/favicon-32x32.ico";
+  import profile from "$lib/assets/images/profilephoto.jpg";
 </script>
 
 <Nav/>
-<div>
-  <h1>Player Name</h1>
+<div id="playerDiv1" class="text-center">
+  <h1>Player Name Goes Here</h1>
 </div>
 
-<div id="recentResultsAndGames"  >
+<div id="playerDiv2"  >
   <!-- TODO add scoreboard thats displays teams and match scores -->
   
   <!-- SCOREBOARD -->
   <div id="recentResultsScoreboardContainer" >
-
   <div id="scoreboardTeamsAndScore" class=" flex flex-row text justify-center text-center space-x-6">
     <!-- TEAM 1 RESULTS -->
     <div id="team1">
       <!-- TODO add image that dynamically generates depending on players team -->
       <h2>Team 1 Name or Logo</h2>
-      <p class="bg-white text-black">Score 4</p>
+      <textarea class="bg-white text-black text-center" placeholder="score" readonly></textarea>
     </div>    
 
       <div>
@@ -36,69 +36,34 @@
     <div id="team2">
       <!-- TODO add image that dynamically generates depending on players team -->
       <h2>Team 2 Name or Logo</h2>
-      <p class="bg-white text-black">Score 3</p>
+      <textarea class="bg-white text-black text-center" placeholder="score" readonly></textarea>
     </div>
   </div>
+</div>
+</div>
 
+<div id="playerDiv3" class="flex flex-row">
+  <div class="bg-red-100 mt-24 w-6/12">
+    <h1 class="text-center">[Player]'s Bio</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia explicabo reiciendis ducimus quidem facere fuga. Facere excepturi rerum dolores! Delectus explicabo qui asperiores deleniti vel, nesciunt dolorem fuga laudantium aliquam?</p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi quo harum facere consequuntur quos sunt, repudiandae at accusamus beatae quaerat amet, temporibus laboriosam, corrupti quasi! Eos sed libero quo quam?
+    </p>
   </div>
-    
 
-<!-- RECENT PLAYER STATS -->
-  <div class="overflow-x-auto">
-  <table class="table table-zebra">
-    <!-- head -->
-    <thead>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-      </tr>
-    </thead>
-    <tbody>
-      <!-- row 1 -->
-      <tr>
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
-      </tr>
-      <!-- row 2 -->
-      <tr>
-        <th>2</th>
-        <td>Hart Hagerty</td>
-        <td>Desktop Support Technician</td>
-        <td>Purple</td>
-      </tr>
-      <!-- row 3 -->
-      <tr>
-        <th>3</th>
-        <td>Brice Swyre</td>
-        <td>Tax Accountant</td>
-        <td>Red</td>
-      </tr>
-    </tbody>
-  </table>
+  <div id="photoAndStatsContainer" class=""> </div>
+  <div id="playerProfilePhoto" class="w-4/12">
+  <!-- TODO add image that dynamically loads a players selected photo -->
+    <img class="rounded-full" src={profile}/> 
 </div>
-</div>
+<!--   
+  <div id="playerStatsContainer">
+    <div>
+      <h2>Player Stats</h2>
+      <p>Player Stats</p>
+    </div>
+  </div> -->
 
-<div id="playerBioContainer">
-  <div>
-    <h2>Player Bio</h2>
-    <p>Player Bio</p>
-  </div>
-</div>
-
-<div id="playerProfilePhoto">
-<!-- TODO add image that dynamically loads a players selected photo -->
-</div>
-
-
-<div id="playerStatsContainer">
-  <div>
-    <h2>Player Stats</h2>
-    <p>Player Stats</p>
-  </div>
 </div>
 
 <Footer/>
