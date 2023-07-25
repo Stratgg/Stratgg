3661c99f86ab1de9b9b93fcfc8f49b913a286d26
Marshall Burns <marshallbcodes@gmail.com>
2023-07-25 13:28:07 -0500

    Add temp assets/minor changes to player profile

--- client/src/routes/esports/[player_id]/+page.svelte
+++ client/src/routes/esports/[player_id]/+page.svelte
@@ -8,11 +8,13 @@
 	import Nav from "$lib/components/Nav.svelte";
   import favicon from "$lib/assets/favicons/favicon-32x32.ico";
   import profile from "$lib/assets/images/profilephoto.jpg";
+  import team1logo from "$lib/assets/images/team1logo.png";
+  import team2logo from "$lib/assets/images/team2logo.png";
 </script>
 
 <Nav/>
 <div id="playerDiv1" class="text-center">
-  <h1 class="text-6xl mb-10">Jonh Doe</h1>
+  <h1 class="text-7xl mb-10">John Doe</h1>
 </div>
 
 <div id="playerDiv2" class="flex flex-col text-center"   >
@@ -20,24 +22,26 @@
   
   <!-- SCOREBOARD -->
   <div id="recentResultsScoreboardContainer"  >
-    <h1>Recent Match Results</h1>
+    <h1 class="text-2xl mb-3">Recent Match Results</h1>
   <div id="scoreboardTeamsAndScore" class=" flex flex-row text justify-center text-center space-x-6">
     <!-- TEAM 1 RESULTS -->
     <div id="team1">
       <!-- TODO add image that dynamically generates depending on players team -->
-      <h2>Team 1 Name or Logo</h2>
-      <textarea class="bg-white text-black text-center resize-none w-6/12 rounded" placeholder="score" readonly></textarea>
+      <h2>Axemen</h2>
+      <img class="object-contain h-24 w-48" src={team1logo}/>
+      <textarea class="text-black text-center resize-none w-6/12 rounded" placeholder="score" readonly></textarea>
     </div>    
 
       <div class="m-auto">
-        <h1>VS</h1>
+        <h1 class="text-5xl">VS</h1>
       </div>
       
     <!-- TEAM 2 RESULTS -->
-    <div id="team2">
+    <div id="team2" >
       <!-- TODO add image that dynamically generates depending on players team -->
-      <h2>Team 2 Name or Logo</h2>
-      <textarea class="bg-white text-black text-center resize-none w-6/12 rounded" placeholder="score" readonly></textarea>
+      <h2>Dragons</h2>
+      <img class="object-contain h-24 w-48" src={team2logo}/>
+      <textarea class="text-black text-center resize-none w-6/12 rounded" placeholder="score" readonly></textarea>
     </div>
   </div>
 </div>
@@ -45,7 +49,7 @@
 
 <div id="playerDiv3" class="flex flex-row">
   <div class="mt-24 w-6/12">
-    <h1 class="text-center">[Player]'s Bio</h1>
+    <h1 class="text-5xl mb-4">Bio:</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia explicabo reiciendis ducimus quidem facere fuga. Facere excepturi rerum dolores! Delectus explicabo qui asperiores deleniti vel, nesciunt dolorem fuga laudantium aliquam?</p>
     <p>
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi quo harum facere consequuntur quos sunt, repudiandae at accusamus beatae quaerat amet, temporibus laboriosam, corrupti quasi! Eos sed libero quo quam?
@@ -59,12 +63,20 @@
     </div>
     <div id="playerStatsContainer" >
       <div class="text-center">
-        <h2>[Player]'s Stats</h2>
-        <p>Rating</p>
-        <p>Eliminations</p>
-        <p>Death</p>
-        <p>Assists</p>
-        <p>Damage</p>
+        <div id="playerInfo">
+          <h2>John Doe's Information</h2>
+          <p>Team</p>
+          <p>Rating</p>
+          <p>Role</p>
+          <p>Location</p>
+        </div>
+        
+        <div id="playerGameStats">
+          <p>Eliminations</p>
+          <p>Death</p>
+          <p>Assists</p>
+          <p>Damage</p>
+        </div>
       </div>
     </div>
   </div>
