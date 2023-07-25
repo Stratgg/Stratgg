0a0c533c22c14ae3781d41f20f8c477be5ce5d06
Marshall Burns <marshallbcodes@gmail.com>
2023-07-25 12:34:57 -0500

    Changes to scoreboard

--- client/src/routes/esports/[player_id]/+page.svelte
+++ client/src/routes/esports/[player_id]/+page.svelte
@@ -12,23 +12,24 @@
 
 <Nav/>
 <div id="playerDiv1" class="text-center">
-  <h1>Player Name Goes Here</h1>
+  <h1 class="text-6xl mb-10">Jonh Doe</h1>
 </div>
 
-<div id="playerDiv2"  >
+<div id="playerDiv2" class="flex flex-col text-center"   >
   <!-- TODO add scoreboard thats displays teams and match scores -->
   
   <!-- SCOREBOARD -->
-  <div id="recentResultsScoreboardContainer" >
+  <div id="recentResultsScoreboardContainer"  >
+    <h1>Recent Match Results</h1>
   <div id="scoreboardTeamsAndScore" class=" flex flex-row text justify-center text-center space-x-6">
     <!-- TEAM 1 RESULTS -->
     <div id="team1">
       <!-- TODO add image that dynamically generates depending on players team -->
       <h2>Team 1 Name or Logo</h2>
-      <textarea class="bg-white text-black text-center resize-none" placeholder="score" readonly></textarea>
+      <textarea class="bg-white text-black text-center resize-none w-6/12" placeholder="score" readonly></textarea>
     </div>    
 
-      <div>
+      <div class="m-auto">
         <h1>VS</h1>
       </div>
       
@@ -36,14 +37,14 @@
     <div id="team2">
       <!-- TODO add image that dynamically generates depending on players team -->
       <h2>Team 2 Name or Logo</h2>
-      <textarea class="bg-white text-black text-center resize-none" placeholder="score" readonly></textarea>
+      <textarea class="bg-white text-black text-center resize-none w-6/12" placeholder="score" readonly></textarea>
     </div>
   </div>
 </div>
 </div>
 
 <div id="playerDiv3" class="flex flex-row justify-between">
-  <div class="bg-red-100 mt-24 w-6/12">
+  <div class="bg-red-100 w-6/12">
     <h1 class="text-center">[Player]'s Bio</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia explicabo reiciendis ducimus quidem facere fuga. Facere excepturi rerum dolores! Delectus explicabo qui asperiores deleniti vel, nesciunt dolorem fuga laudantium aliquam?</p>
     <p>
@@ -51,10 +52,10 @@
     </p>
   </div>
 
-  <div id="photoAndStatsContainer">
-    <div id="playerProfilePhoto">
+  <div id="photoAndStatsContainer" class="text-center">
+    <div id="playerProfilePhoto" >
     <!-- TODO add image that dynamically loads a players selected photo -->
-      <img class="rounded-full " src={profile}/> 
+      <img class="rounded-full w-6/12 m-auto" src={profile}/> 
     </div>
     <div id="playerStatsContainer" >
       <div class="text-center">
