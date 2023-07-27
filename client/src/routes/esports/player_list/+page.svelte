64ff3a6baf5cd0010a82b9136ba86004fec30abc
Marshall Burns <marshallbcodes@gmail.com>
2023-07-27 10:01:14 -0500

    Create load file/successfully fetch data from db

--- client/src/routes/esports/player_list/+page.svelte
+++ client/src/routes/esports/player_list/+page.svelte
@@ -8,7 +8,18 @@
   import Footer from "$lib/components/Footer.svelte";
 	import Nav from "$lib/components/Nav.svelte";
   import favicon from "$lib/assets/favicons/favicon-32x32.ico";
+  export let data
+  const {playerList} = data
+  console.log(playerList)
 </script>
 
 <Nav/>
+{#each playerList as player}
+  <h1>{player.id}</h1>
+  <h1>{player.first_name}</h1>
+  <h1>{player.last_name}</h1>
+  <h1>{player.rating}</h1>
+  <h1>{player.location}</h1>
+  <h1>{player.game}</h1>
+{/each}
 <Footer/>
