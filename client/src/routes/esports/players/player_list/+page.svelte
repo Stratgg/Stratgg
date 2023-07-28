4c6b1e2990e3c5c53b57e3ff47c0501ddf10f350
Marshall Burns <marshallbcodes@gmail.com>
2023-07-28 09:36:39 -0500

    Attempt to fix merge conflict

--- client/src/routes/esports/players/player_list/+page.svelte
+++ client/src/routes/esports/players/player_list/+page.svelte
@@ -10,16 +10,31 @@
   import favicon from "$lib/assets/favicons/favicon-32x32.ico";
   export let data
   const {playerList} = data
-  console.log(playerList)
 </script>
 
 <Nav/>
-{#each playerList as player}
-  <h1>{player.id}</h1>
-  <h1>{player.first_name}</h1>
-  <h1>{player.last_name}</h1>
-  <h1>{player.rating}</h1>
-  <h1>{player.location}</h1>
-  <h1>{player.game}</h1>
-{/each}
+  <div id="playerListTableContainer" class="overflow-x-auto">
+    <table id="playerListTable" class="table table-zebra">
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Rating</th>
+          <th>Location</th>
+          <th>Game</th>
+        </tr>
+      </thead>
+      <tbody >
+          {#each playerList as player}
+          <tr>
+            <td><a class="block" href="player_id">{player.first_name}</a></td>
+            <td><a class="block" href="player_id">{player.last_name}</a></td>
+            <td><a class="block" href="player_id">{player.rating}</td>
+            <td><a class="block" href="player_id">{player.location}</td>
+            <td><a class="block" href="player_id">{player.game}</td>
+            </tr>
+          {/each}    
+      </tbody>  
+    </table>
+  </div>
 <Footer/>
