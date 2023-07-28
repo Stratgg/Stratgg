da92482d3912214c0c50479bddb5e4352661c703
Marshall Burns <marshallbcodes@gmail.com>
2023-07-27 21:24:04 -0500

    Update route route in FastifyInstance

--- server/src/routes/Players.ts
+++ server/src/routes/Players.ts
@@ -2,7 +2,7 @@ import { PlayerEntity } from '../entity/PlayerEntity'
 import type { FastifyInstance } from 'fastify'
 
 export async function players(instance: FastifyInstance) {
-	instance.get(':id', async (_, reply) => {
+	instance.get('/:id', async (_, reply) => {
 		const result = await PlayerEntity.find({})
 		return result
 	})
