523dc2ce478ac3d2cae9073451c0b3273dc32f5c
Marshall Burns <marshallbcodes@gmail.com>
2023-07-28 09:25:32 -0500

    Add where statement/fix data fetching!!!

--- server/src/routes/Players.ts
+++ server/src/routes/Players.ts
@@ -3,7 +3,9 @@ import type { FastifyInstance } from 'fastify'
 
 export async function players(instance: FastifyInstance) {
 	instance.get('/:id', async (_, reply) => {
-		const result = await PlayerEntity.find({})
+		const result = await PlayerEntity.find({ where: [{ id: _.params.id }] })
+		// console.log(_.params)
+
 		return result
 	})
 }
