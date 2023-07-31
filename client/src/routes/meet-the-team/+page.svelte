97556e741d3f9b0d62a1877508dc0566c36bdfab
Marshall Burns <marshallbcodes@gmail.com>
2023-07-31 13:37:09 -0500

    Start work on "meet the team" page

---
+++ client/src/routes/meet-the-team/+page.svelte
@@ -0,0 +1,30 @@
+<svelte:head>
+  <title>StratGG / The Team</title>
+  <link rel="icon" href={favicon} />
+</svelte:head>
+
+<script>
+import favicon from "$lib/assets/images/favicons/apple-touch-icon.png";
+import Nav from "$lib/components/Nav.svelte";
+import Footer from "$lib/components/Footer.svelte";
+import schooly from "$lib/assets/images/stratgg-team-avatars/schooly.jpg";
+</script>
+
+<Nav/>
+<div id="teamContainer" class="flex flex-col text-center" >
+  <div id="marshallContainer">
+    <div class="avatar" id="schoolyAvatar">
+      <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+        <img src={schooly}/>
+      </div>
+    </div>
+    <div id="marshallBio" class="text-center">
+    <h1>Marshall Burns</h1>
+    <h1>Developer</h1>
+    <p>Overwatch • Minecraft • Valorant</p>
+    </div>
+  </div>  
+</div>
+  
+<Footer/>
+t
