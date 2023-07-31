414678420656aa9dafdd12f6a17a5322bc71deea
Marshall Burns <marshallbcodes@gmail.com>
2023-07-31 15:42:08 -0500

    Minor changes/add assets

--- client/src/routes/meet-the-team/+page.svelte
+++ client/src/routes/meet-the-team/+page.svelte
@@ -9,16 +9,17 @@ import Nav from "$lib/components/Nav.svelte";
 import Footer from "$lib/components/Footer.svelte";
 import marshall from "$lib/assets/images/stratgg-team-avatars/schooly.jpg";
 import jasmine from "$lib/assets/images/stratgg-team-avatars/jazz.jpg";
+import tre from "$lib/assets/images/stratgg-team-avatars/tre.jpg";
 import basic from "$lib/assets/images/stratgg-team-avatars/default.svg";
 </script>
 
 <Nav/>
 <div id="teamContainer" class="flex flex-col text-center justify-center">
   <div class="flex flex-row justify-center m-auto" id="teamTopRow">
-    <div id="thayerContainer" class="m-10 bg-secondary border border-solid rounded-2xl w-96 h-96" >
+    <div id="thayerContainer" class="m-10 bg-secondary rounded-2xl w-96 h-96 hover:cursor-pointer hover:shadow-black hover: shadow-2xl hover:bg-slate-600"  >
     <div class="avatar mt-20" id="thayerAvatar">
       <div class="w-40 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-        <img  src={basic}/>
+        <img src={tre}/>
       </div>
     </div>
     <div id="thayerBio" class="text-center">
@@ -28,7 +29,7 @@ import basic from "$lib/assets/images/stratgg-team-avatars/default.svg";
     </div>
   </div>
    
-  <div id="kolebeContainer" class="m-auto bg-secondary border border-solid rounded-2xl w-96 h-96">
+  <div id="kolebeContainer" class="m-10 bg-secondary rounded-2xl w-96 h-96 hover:cursor-pointer hover:shadow-black hover: shadow-2xl hover:bg-slate-600">
     <div class="avatar mt-20" id="kolebeAvatar">
       <div class="w-40 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
         <img src={basic}/>
@@ -41,7 +42,7 @@ import basic from "$lib/assets/images/stratgg-team-avatars/default.svg";
     </div>
   </div>
 
-  <div id="marshallContainer" class="m-10 bg-secondary border border-solid rounded-2xl w-96 h-96">
+  <div id="marshallContainer" class="m-10 bg-secondary rounded-2xl w-96 h-96 hover:cursor-pointer hover:shadow-black hover: shadow-2xl hover:bg-slate-600">
     <div class="avatar mt-20" id="marshallAvatar">
       <div class="w-40 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
         <img src={marshall}/>
@@ -58,7 +59,7 @@ import basic from "$lib/assets/images/stratgg-team-avatars/default.svg";
   
   
   <div class="flex flex-row justify-center m-auto" id="teamBottomRow">
-    <div id="lacyContainer" class="m-10 bg-secondary border border-solid rounded-2xl w-96 h-96">
+    <div id="lacyContainer" class="m-10 bg-secondary rounded-2xl w-96 h-96 hover:cursor-pointer hover:shadow-black hover: shadow-2xl hover:bg-slate-600">
     <div class="avatar mt-20" id="lacyAvatar">
       <div class="w-40 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
         <img src={basic}/>
@@ -71,7 +72,7 @@ import basic from "$lib/assets/images/stratgg-team-avatars/default.svg";
     </div>
   </div>
   
-  <div id="jasmineContainer" class="m-10 bg-secondary border border-solid rounded-2xl w-96 h-96">
+  <div id="jasmineContainer" class="m-10 bg-secondary rounded-2xl w-96 h-96 hover:cursor-pointer hover:shadow-black hover: shadow-2xl hover:bg-slate-600">
     <div class="avatar mt-20" id="jasmineAvatar">
       <div class="w-40 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
         <img src={jasmine}/>
