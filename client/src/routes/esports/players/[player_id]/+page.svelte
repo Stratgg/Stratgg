51d8a74fe369331ff4cbd651fdb0f17820b438d7
Marshall Burns <marshallbcodes@gmail.com>
2023-07-31 20:46:54 -0500

    More changes to player profile

--- client/src/routes/esports/players/[player_id]/+page.svelte
+++ client/src/routes/esports/players/[player_id]/+page.svelte
@@ -17,18 +17,89 @@
 </script>
 
 <Nav/>
-<div id="playerDiv1" class="text-center">
-  <h1 class="text-5xl mb-4">"{player.gamer_tag}"</h1>
-  <h1 class="text-7xl mb-10">{player.first_name} {player.last_name}</h1>
+<div id="playerAvatarContainer" class="flex justify-center">
+  <div class="avatar " >
+    <div class="w-60 rounded-full">
+      <img src={avatar} />
+    </div>
+  </div>
 </div>
+
+
+<div id="bioAndStatsContainer" class="flex flex-row justify-between">
+  <!-- BIO -->
+  <div id="bio" >
+    <div class="m-auto ">
+      <h1 class="text-5xl mb-4">About {player.gamer_tag}:</h1>
+      <p>{player.bio}</p>
+    </div>
+  </div>
+  <!-- STATS -->
+  <div class="card bg-base-100 shadow-xl">
+    
+    <div class="card card-compact w-96 bg-base-100 ">
+  
+  <div class="card-body">
+    <h2 class="card-title justify-center text-3xl">{player.first_name} {player.last_name}</h2>
+    <h3 class="card-title justify-center">"{player.gamer_tag}"</h3>
+    <h2 class="card-title justify-center">{player.location}</h2>
+    
+    <h2 class="card-title justify-center">{player.game}</h2>
+    <h2 class="card-title justify-center">Role</h2>
+    <h2 class="card-title justify-center">Operator/Hero/Champion/Agent</h2>
+  </div>
+</div>
+    <figure><img class="w-32" src={team1logo} alt="TEAM" /></figure>
+    
+    <div class="stat place-items-center">
+      <div class="stat-value">Axemen</div>
+    </div>  
+   <div class="stats stats-vertical lg:stats-horizontal shadow">
+  <div class="stat place-items-center">
+    <div class="stat-title">Eliminations</div>
+    <div class="stat-value">322</div>
+    </div>
+  
+  <div class="stat place-items-center">
+    <div class="stat-title">Deaths</div>
+    <div class="stat-value">231</div>
+  </div>
+  
+  <div class="stat place-items-center">
+    <div class="stat-title">Assists</div>
+    <div class="stat-value">99</div>
+  </div>
+  
+</div>
+ <div class="stats stats-vertical lg:stats-horizontal shadow">
+  <div class="stat place-items-center">
+    <div class="stat-title">Wins</div>
+    <div class="stat-value">22</div>
+    </div>
+  
+  <div class="stat place-items-center">
+    <div class="stat-title">Losses</div>
+    <div class="stat-value">19</div>
+  </div>
+  
+  <div class="stat place-items-center">
+    <div class="stat-title">Draws</div>
+    <div class="stat-value">2</div>
+  </div>
+  
+</div>
+
+  </div>
+</div>
+<!-- SCOREBOARD -->
 <div id="playerDiv2" class="flex flex-col text-center"   >
   <!-- TODO add scoreboard thats displays teams and match scores -->
   
   <!-- SCOREBOARD -->
-  <div id="recentResultsScoreboardContainer"  >
-    <h1 class="text-2xl mb-3">Recent Match Results</h1>
-  <div id="scoreboardTeamsAndScore" class=" flex flex-row text justify-center text-center space-x-6">
-    <!-- TEAM 1 RESULTS -->
+  <div id="recentResultsScoreboardContainer" class=" w-6/12 m-auto rounded-3xl bg-secondary"  >
+    <h1 class="text-2xl mb-3">Recent Match</h1>
+    <div id="scoreboardTeamsAndScore" class=" flex flex-row text justify-center text-center space-x-6">
+      <!-- TEAM 1 RESULTS -->
     <div id="team1">
       <!-- TODO add image that dynamically generates depending on players team -->
       <h2>Axemen</h2>
@@ -38,7 +109,7 @@
       <div class="m-auto">
         <h1 class="text-5xl">VS</h1>
       </div>
-      
+            
     <!-- TEAM 2 RESULTS -->
     <div id="team2" >
       <!-- TODO add image that dynamically generates depending on players team -->
@@ -48,41 +119,7 @@
     </div>
   </div>
 </div>
-</div>
-<div id="playerDiv3" class="flex flex-row justify-between">
-  <div class="mt-24 w-6/12">
-    <h1 class="text-5xl mb-4">Bio:</h1>
-    <p>{player.bio}
-    </p>
-  </div>
 
-  <div id="photoAndStatsContainer" class="text-center ">
-    <div class="card w-80 bg-base-100 shadow-xl">
-      <figure><img src={avatar}/> </figure>
-      <div class="card-body">
-        <h2 class="text-center">{player.first_name} {player.last_name} </h2>
-        <p>Team</p>
-        <p>{player.rating}</p>
-        <p>Role</p>
-        <p>{player.location}</p>
-          <div class="stats stats-vertical">
-            <div class="stat">
-              <div class="stat-title">All Time Eliminations</div>
-              <div class="stat-value">100</div>
-            </div>
-            
-            <div class="stat">
-              <div class="stat-title">All Time Death</div>
-              <div class="stat-value">25</div>
-            </div>
-            
-            <div class="stat">
-              <div class="stat-title">All Time Assist</div>
-              <div class="stat-value">18</div>
-            </div>  
-          </div>
-        </div>
-      </div>
-    </div>
 </div>
+
 <Footer/>
