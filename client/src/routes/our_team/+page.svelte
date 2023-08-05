347fef94b94de10bc825086e48c6d30dce488680
Marshall Burns <marshallbcodes@gmail.com>
2023-08-04 21:06:05 -0500

    Update our team page/add asset

--- client/src/routes/our_team/+page.svelte
+++ client/src/routes/our_team/+page.svelte
@@ -10,6 +10,7 @@ import Footer from "$lib/components/Footer.svelte";
 import marshall from "$lib/assets/images/stratgg-team-avatars/schooly.jpg";
 import jasmine from "$lib/assets/images/stratgg-team-avatars/jazz.jpg";
 import tre from "$lib/assets/images/stratgg-team-avatars/tre.jpg";
+import gnome from "$lib/assets/images/stratgg-team-avatars/gnome.png";
 import basic from "$lib/assets/images/stratgg-team-avatars/default.svg";
 </script>
 
@@ -25,7 +26,7 @@ import basic from "$lib/assets/images/stratgg-team-avatars/default.svg";
     <div id="thayerBio" class="text-center">
     <h1>Thayer Weaver III</h1>
     <h1>Founder</h1>
-    <p>Stardew Valley</p>
+    <p>Halo 3</p>
     </div>
   </div>
    
@@ -62,13 +63,13 @@ import basic from "$lib/assets/images/stratgg-team-avatars/default.svg";
     <div id="lacyContainer" class="m-10 bg-secondary rounded-2xl w-96 h-96 hover:cursor-pointer hover:shadow-black hover: shadow-2xl hover:bg-slate-600 text-white">
     <div class="avatar mt-20" id="lacyAvatar">
       <div class="w-40 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-        <img src={basic}/>
+        <img src={gnome}/>
       </div>
     </div>
     <div id="lacyBio" class="text-center">
     <h1>Lacy Randolph</h1>
     <h1>Developer • Tester</h1>
-    <p>Stardew Valley </p>
+    <p>Stardew Valley • Deep Rock Galactic</p>
     </div>
   </div>
   
