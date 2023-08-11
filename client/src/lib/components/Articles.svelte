eb890d746b46408cf7cbd690653b12274955285e
Marshall Burns <marshallbcodes@gmail.com>
2023-08-11 12:01:18 -0500

    remove lines

--- client/src/lib/components/Articles.svelte
+++ client/src/lib/components/Articles.svelte
@@ -1,13 +1,8 @@
-<script>
-	import keyboard from "$lib/assets/images/homepage/gaming-keyboard.png"
-	import gamer from "$lib/assets/images/homepage/cyberpunk-gamer.jpg"
-</script>
-
 <!-- Container for demo purpose -->
 <div class="container my-24 mx-auto md:px-6">
 	<!-- Section: Design Block -->
-	<section class="mb-32 text-center">
-		<h2 class="mb-12 text-center text-5xl font-bold">Latest Articles</h2>
+	<section class="mb-32 ">
+		<h2 class="mb-12 text-center text-6xl font-bold">Latest Articles</h2>
 
 		<div class="grid gap-6 lg:grid-cols-3 xl:gap-x-12">
 			<div class="mb-6 lg:mb-0">
@@ -109,118 +104,5 @@
 			</div>
 		</div>
 	</section>
-	<section class="mb-32 text-center">
-
-		<div class="grid gap-6 lg:grid-cols-3 xl:gap-x-12">
-			<div class="mb-6 lg:mb-0">
-				<div
-					class="relative mb-6 overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
-					data-te-ripple-init
-					data-te-ripple-color="light"
-				>
-					<img
-						src={keyboard}
-						class="w-full"
-						alt="Louvre"
-					/>
-					<a href="#!">
-						<div
-							class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100 bg-[hsla(0,0%,98.4%,.15)]"
-						/>
-					</a>
-				</div>
-
-				<h5 class="mb-3 text-lg font-bold">
-					Trailing behind, the Raging Tigers find a ray of hope in their prodigious player,
-					"Tempest," whose game-changing maneuver brings the Tigers back into the fray, promising a
-					riveting match ahead.
-				</h5>
-				<p class="mb-6 text-neutral-500 dark:text-neutral-300">
-					<small
-						>Published <u>13.01.2024</u> by
-						<a href="#!">Anna Maria Doe</a></small
-					>
-				</p>
-				<p class="text-neutral-500 dark:text-neutral-300">
-					Trailing behind, the Raging Tigers find a ray of hope in their prodigious player,
-					"Tempest," whose game-changing maneuver brings the Tigers back into the fray, promising a
-					riveting match ahead.
-				</p>
-			</div>
-
-			<div class="mb-6 lg:mb-0">
-				<div
-					class="relative mb-6 overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
-					data-te-ripple-init
-					data-te-ripple-color="light"
-				>
-					<img
-						src={gamer}
-						class="w-full"
-						alt="Louvre"
-					/>
-					<a href="#!">
-						<div
-							class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100 bg-[hsla(0,0%,98.4%,.15)]"
-						/>
-					</a>
-				</div>
-
-				<h5 class="mb-3 text-lg font-bold">
-					The "Rising Phoenix League 2023" pits the unstoppable Team Onslaught from the U.S against
-					the fearless Raging Tigers from India, in a gripping encounter on the eSports battlefield
-					of 'Galactic Frontiers.
-				</h5>
-
-				<p class="mb-6 text-neutral-500 dark:text-neutral-300">
-					<small
-						>Published <u>12.01.2022</u> by
-						<a href="#!">Halley Frank</a></small
-					>
-				</p>
-				<p class="text-neutral-500 dark:text-neutral-300">
-					The "Rising Phoenix League 2023" pits the unstoppable Team Onslaught from the U.S against
-					the fearless Raging Tigers from India, in a gripping encounter on the eSports battlefield
-					of 'Galactic Frontiers.'
-				</p>
-			</div>
-
-			<div class="mb-0">
-				<div>
-					<div
-						class="relative mb-6 overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
-						data-te-ripple-init
-						data-te-ripple-color="light"
-					>
-						<img
-							src="https://mdbcdn.b-cdn.net/img/new/standard/city/059.jpg"
-							class="w-full"
-							alt="Louvre"
-						/>
-						<a href="#!">
-							<div
-								class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100 bg-[hsla(0,0%,98.4%,.15)]"
-							/>
-						</a>
-					</div>
-
-					<h5 class="mb-3 text-lg font-bold">
-						The Miracle Match: Raging Tigers' Unforgettable Comeback Shakes eSports World
-					</h5>
-					<p class="mb-6 text-neutral-500 dark:text-neutral-300">
-						<small
-							>Published <u>10.01.2022</u> by
-							<a href="#!">Joe Svan</a></small
-						>
-					</p>
-					<p class="text-neutral-500 dark:text-neutral-300">
-						In a nail-biting climax, Raging Tigers, backed by Tempest's heroics, pull off a
-						miraculous comeback, sending ripples through the eSports world and cementing their
-						legacy as one of the greatest underdog stories ever.
-					</p>
-				</div>
-			</div>
-		</div>
-	</section>
-	<!-- Section: Design Block -->
+	<section class="mb-32">
 </div>
--- client/src/routes/contact_us/+page.svelte
+++ client/src/routes/contact_us/+page.svelte
@@ -33,11 +33,10 @@ const handleSubmit = async function(data:any) {
 </script>
 
   <Nav/> 
-
   <section class="dark:bg-gray-900">
     <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-      <h1 class="mb-4 text-4xl tracking-tight font-extrabold text-center dark:text-white">Contact Us</h1>
-      <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Need account help? Have ideas for improvements? Let us know.</p>
+      <h1 class="mb-4 text-6xl tracking-tight font-extrabold text-center dark:text-white">Contact Us</h1>
+      <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Have any technical issue, questions, or suggestions? Let us know.</p>
 
  
       <form on:submit|preventDefault={handleSubmit} class="space-y-8">
@@ -52,16 +51,18 @@ const handleSubmit = async function(data:any) {
         </div>
         <div class="sm:col-span-2">
         <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Message </label>
-        <textarea name="message" required rows="3" placeholder="Message" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"></textarea>
+        <textarea name="message" required rows="8" placeholder="Message" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"></textarea>
         </div>
-        <input type="submit" class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"/>
+        <input type="submit" class="py-3 px-5 text-md font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800  focus:border bg-secondary"/>
       </form>
       </div>
       </section>
-    <!-- </div> -->
-  
-  <!-- </div> -->
-
-<div>{status}</div>
+      <div class="text-center">
+        <div>
+          <h1 class="text-2xl">
+            {status}
+          </h1>
+        </div>
+      </div>
 
   <Footer/> 
